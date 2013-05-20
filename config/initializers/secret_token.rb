15993b85baa0d16d650108aa8db680d35eeebb8c
Jen <jenstrumental@gmail.com>
2013-05-20 13:04:26 -0700

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wombat::Application.config.secret_key_base = '02c9154947a90da9e158a3d3785717c4bcb6bde7281a38803a1ead1e9ebb8ecba3f33abfc102d436d997e895b1517165ce0ad41ae8b24a653a9caaa3836f8d91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wombat::Application.config.session_store :cookie_store, key: '_wombat_session'
